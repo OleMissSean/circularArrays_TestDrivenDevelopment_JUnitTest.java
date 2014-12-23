cd9015f604f2359ac39093c2e99be526c843ec94
Sean Staz <seanstaz52@gmail.com>
2014-12-23 00:50:50 -0600

    Create E1.java

---
+++ E1.java
@@ -0,0 +1,22 @@
+/* 
+     Name:  Sean Staz
+     Email:  seanstaz52@gmail.com
+     Current Date:  9/17/2013
+     Class: CSci 211 Section 1 Fall 2013
+     Sources Consulted: None. 
+     
+     Honor Code Statement: In keeping with the honor code policies of the University of Mississippi, the School of Engineering, and the Department of Computer and Information Science, I affirm that I have neither given nor received assistance on this programming assignment. This assignment represents my individual, original effort. 
+                    ... My Signature is on File. 
+*/ 
+
+package e1;
+
+public class E1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.printf("Hello World!! x=%d\n", 5);
+	}
+
+}
