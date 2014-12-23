8beba5456ffdc35df889763245a5c1813b1a7275
Sean Staz <seanstaz52@gmail.com>
2014-12-23 00:54:12 -0600

    Update CircularArray.java

--- CircularArray.java
+++ CircularArray.java
@@ -1,6 +1,13 @@
-/**
- * 
- */
+/*
+Name: Sean Staz
+Email: seanstaz52@gmail.com
+Current Date: 9/17/2013
+Class: CSci 211 Section 1 Fall 2013
+Sources Consulted: None.
+Honor Code Statement: In keeping with the honor code policies of the University of Mississippi, the School of Engineering, and the Department of Computer and Information Science, I affirm that I have neither given nor received assistance on this programming assignment. This assignment represents my individual, original effort.
+... My Signature is on File.
+*/ 
+
 package e1;
 
 import java.nio.BufferOverflowException;
